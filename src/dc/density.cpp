68b0d4f1e162dfb893f36738f7a84df0a1cc0c75
Caleb Daniels <Metalith68@gmail.com>
2016-07-02 18:57:51 -0600

    Improve Performance of Chunk Generation

--- src/dc/density.cpp
+++ src/dc/density.cpp
@@ -9,14 +9,43 @@ DensityField::DensityField(vec3 pos, int size, float LOD) {
 	this->size = size;	
 	this->LOD = LOD;
 	this->pos = pos;
-	corner corner;
+	Point point;
+	vec3 p, n, tmp;
 	for (float i = -size; i <= size; i+=LOD) {
 		for (float j = -size; j <= size; j+=LOD) {
 			for (float k = -size; k <= size; k+=LOD) {
-				float density = SDF(pos +  vec3(i,j,k));
+				tmp = this->pos + vec3(i, j, k);
+				float density = SDF(tmp);
 				char material = density < 0 ? 1 : 0;
-				corner.point = material;
-				points.push_back(corner);
+				point.material = material;
+				if (points.size() > 0) {
+					if (material != points [points.size() - 1].material) {
+						p = ApproximateZeroCrossingPosition(tmp - vec3(0,0,1), tmp);
+						n = CalculateSurfaceNormal(p);
+						point.edges[2] = edges.size();
+						edges.push_back(p);
+						edges.push_back(n);
+					}
+				}
+				if (points.size() > size * 2 *1.0f / LOD + 1) {
+					if (material != points [points.size() - (size * 2 *1.0f / LOD + 1)].material) {
+						p = ApproximateZeroCrossingPosition(tmp - vec3(0,1,0), tmp);
+						n = CalculateSurfaceNormal(p);
+						point.edges[1] = edges.size();
+						edges.push_back(p);
+						edges.push_back(n);
+					}
+				}
+				if (points.size() > size * 2 *1.0f / LOD + 1) {
+					if (material != points [points.size() - ((size * 2 *1.0f / LOD + 1) * (size * 2 *1.0f / LOD + 1))].material) {
+						p = ApproximateZeroCrossingPosition(tmp - vec3(1,0,0), tmp);
+						n = CalculateSurfaceNormal(p);
+						point.edges[0] = edges.size();
+						edges.push_back(p);
+						edges.push_back(n);
+					}
+				}
+				points.push_back(point);
 			}
 		}
 	}
@@ -25,7 +54,18 @@ DensityField::DensityField(vec3 pos, int size, float LOD) {
 char DensityField::getPoint(vec3 pos) {
 	int arraySize = size * 2 * 1.0f / LOD + 1;
 	ivec3 newPos = ivec3(((pos - this->pos) + float(this->size)) * (1.0f / LOD));
-	return points[newPos.z + newPos.y * arraySize + newPos.x * arraySize * arraySize].point;
+	return points[newPos.z + newPos.y * arraySize + newPos.x * arraySize * arraySize].material;
+}
+
+std::pair<vec3, vec3> DensityField::getEdge(const vec3& p0, const vec3& p1) {
+	int arraySize = size * 2 * 1.0f / LOD + 1;
+	ivec3 newPos = ivec3(((p1 - this->pos) + float(this->size)) * (1.0f / LOD));
+	int index = newPos.z + newPos.y * arraySize + newPos.x * arraySize * arraySize;
+	Point p = points[index];
+	ivec3 dir = ivec3(p1 - p0);
+	if (dir.z == 1) return std::make_pair(edges[p.edges[2]], edges[p.edges[2] + 1]);
+	if (dir.y == 1) return std::make_pair(edges[p.edges[1]], edges[p.edges[1] + 1]);
+	return std::make_pair(edges[p.edges[0]], edges[p.edges[0] + 1]);
 }
 
 float DensityField::SDF(vec3 pos) { return this->module.GetValue(pos.x / 32, 0.0, pos.z / 32) + ( pos.y / 32); }
--- src/dc/density.hpp
+++ src/dc/density.hpp
@@ -13,6 +13,7 @@ class DensityField {
 	public:
 		DensityField(vec3 pos, int size, float LOD);
 		char getPoint(vec3 pos);
+		std::pair<vec3, vec3> getEdge(const vec3& p0, const vec3& p1);
 		float SDF(vec3 pos);
 	private:
 		int size;
@@ -22,12 +23,13 @@ class DensityField {
 
 		module::Perlin module;
 		
-		struct corner {
-			char point;
-			vec3 edges[3][2];	// -X, -Y, -Z - P, N
+		struct Point {
+			char material;
+			int edges[3] {-1, -1, -1};	// -X, -Y, -Z
 		};
 
-		std::vector<corner> points;
+		std::vector<Point> points;
+		std::vector<vec3> edges;
 		vec3 CalculateSurfaceNormal(const vec3& p);
 		vec3 ApproximateZeroCrossingPosition(const vec3& p0, const vec3& p1);
 };

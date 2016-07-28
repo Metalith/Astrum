623aaf63337fdd1ee549f4b339fec6414891502a
Caleb Daniels <Metalith68@gmail.com>
2016-07-27 19:17:52 -0600

    Add Color Values

--- tools/Apollo/js/color.js
+++ tools/Apollo/js/color.js
@@ -1,9 +1,9 @@
 $( "#color_text" ).focus(function() {
-  $(".ColorPickerBox").toggle(true);
+    $(".ColorPickerBox").toggle(true);
 }).hover(function() {
-  $(".ColorPickerBox").toggle(true);
+    $(".ColorPickerBox").toggle(true);
 }).click(function() {
-  $(".ColorPickerBox").toggle(true);
+    $(".ColorPickerBox").toggle(true);
 });
 
 $(".Color").mouseleave(function() {
@@ -27,6 +27,7 @@ $('.SLPicker').mousedown(function(e) {
     $('.Node #L > input').val(
         ("00" + Math.floor(100 - (e.pageY - $(this).offset().top) * (100 / 256))).slice(-3)
     );
+    updateRGB();
     $(window).bind("mousemove", updateSL);
 }).mouseup(function() {
     $(window).unbind("mousemove", updateSL);
@@ -48,6 +49,7 @@ $('.HPicker').mousedown(function(e) {
         ("00" + Math.floor((e.pageY - $(this).offset().top) * (360 / 256))).slice(-3)
     );
     drawSVPicker($('.SLPicker')[0], (e.pageY - $(this).offset().top) * (360 / 256));
+    updateRGB();
     $(window).bind("mousemove", updateHue);
 }).mouseup(function() {
     $(window).unbind("mousemove", updateHue);
@@ -55,9 +57,9 @@ $('.HPicker').mousedown(function(e) {
 
 function updateHue(e) {
     if (e.pageY - $('.HPicker').offset().top <= 256.0 &&
-        e.pageY - $('.HPicker').offset().top >= 0 &&
-        e.pageX - $('.HPicker').offset().left >= 0 &&
-        e.pageX - $('.HPicker').offset().left <= 32) {
+    e.pageY - $('.HPicker').offset().top >= 0 &&
+    e.pageX - $('.HPicker').offset().left >= 0 &&
+    e.pageX - $('.HPicker').offset().left <= 32) {
         selector = $('.HPicker').next();
         selector.offset({ top: e.pageY - 10, left: e.pageX - 10 });
         $('.Preview').css(
@@ -71,6 +73,7 @@ function updateHue(e) {
             ("00" + Math.floor((e.pageY - $('.HPicker').offset().top) * (360 / 256))).slice(-3)
         );
         drawSVPicker($('.SLPicker')[0], (e.pageY - $('.HPicker').offset().top) * (360 / 256));
+        updateRGB();
     } else {
         $(window).unbind("mousemove", updateHue);
     }
@@ -78,9 +81,9 @@ function updateHue(e) {
 
 function updateSL(e) {
     if (e.pageY - $('.SLPicker').offset().top <= 256.0 &&
-        e.pageY - $('.SLPicker').offset().top >= 0 &&
-        e.pageX - $('.SLPicker').offset().left >= 0 &&
-        e.pageX - $('.SLPicker').offset().left <= 256) {
+    e.pageY - $('.SLPicker').offset().top >= 0 &&
+    e.pageX - $('.SLPicker').offset().left >= 0 &&
+    e.pageX - $('.SLPicker').offset().left <= 256) {
         selector = $('.SLPicker').next();
         selector.offset({ top: e.pageY - 10, left: e.pageX - 10 })
         $('.Preview').css(
@@ -96,9 +99,49 @@ function updateSL(e) {
         $('.Node #L > input').val(
             ("00" + Math.floor(100 - (e.pageY -$('.SLPicker').offset().top) * (100 / 256))).slice(-3)
         );
+        updateRGB();
     } else {
         $(window).unbind("mousemove", updateSL);
     }
+}
+
+function updateRGB() {
+    var h = parseFloat($('.Node #H > input').val());
+    var s = parseFloat($('.Node #S > input').val());
+    var l = parseFloat($('.Node #L > input').val());
+
+    h /= 360;
+    s /= 100;
+    l /=100;
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+    $('.Node #R > input').val(
+        ("00" + Math.floor(r * 255)).slice(-3)
+    );
+    $('.Node #G > input').val(
+        ("00" + Math.floor(g * 255)).slice(-3)
+    );
+    $('.Node #B > input').val(
+        ("00" + Math.floor(b * 255)).slice(-3)
+    );
 }
 
 function drawSVPicker(canvas, hue) {

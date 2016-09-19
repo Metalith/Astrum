7fa403965e30ae217336234395ea87f9764bf086
Caleb Daniels <Metalith68@gmail.com>
2016-09-18 21:03:41 -0600

    Implement Perlin Noise

--- tool/Atlas/js/components/Node.js
+++ tool/Atlas/js/components/Node.js
@@ -41,7 +41,7 @@ class Node extends React.Component {
             this.props.cons))
     }
     onMouseDown(e) {
-        if (e.target.tagName != "INPUT" && !e.target.classList.contains("Field") && e.target.className != "Handle") {
+        if (e.target.tagName != "INPUT" && !e.target.classList.contains("Field") && !e.target.parentElement.classList.contains("Handle")) {
             this.setState({
                 dragging: true,
                 rel: {
@@ -115,14 +115,15 @@ class ValueNode extends Node {
             type="number"
             onChange={(e) => {
                 this.props.dispatch(Actions.updateNode(
+
                     this.props.id,
                     {Value: (e.target.value.length == 0) ? "0" : e.target.value },
-                    {Value: parseFloat((e.target.value.length == 0) ? "0" : e.target.value)},
+                    {Value: (e.target.value.length == 0) ? "0.0" : parseFloat(e.target.value).toFixed(Math.max(1, (e.target.value.split('.')[1] || []).length))},
                     this.props.cons))}}
-                value={this.props.inputs.Value}
+                value={parseFloat(this.props.inputs.Value)}
                 step="0.01"/>}
     static get input() {return {
-        Value: "10"
+        Value: "10.0"
     }}
     get show() {
         return {
@@ -131,7 +132,7 @@ class ValueNode extends Node {
         }
     }
     static get output() {return {
-        Value: 10.0
+        Value: "10.0"
     }}
 }
 
@@ -140,13 +141,44 @@ class MathNode extends Node {
     constructor(props) {
         super(props)
     }
-
-    getOutputs(inputs) { return { Result: `${inputs.Value1} + ${inputs.Value2}` } }
-    center() { return <input type="number" value={this.props.inputs.Value1 + this.props.inputs.Value2}/> }
+    getFunction (fun) {
+        switch (fun) {
+            case 'Add':
+                return '+';
+            case 'Subtract':
+                return '-';
+            case 'Multiply':
+                return '*';
+            case 'Divide':
+                return '/';
+        }
+        return 'Error';
+    }
+    getOutputs(inputs) {
+        return {
+            Result: `${inputs.Value1}${this.getFunction(inputs.Function)}${inputs.Value2}`
+        }
+    }
+    center() {
+        return <select onChange={(e) => {
+            this.props.dispatch(Actions.updateNode(
+                this.props.id,
+                {Function: e.target.value },
+                {Result:`${this.props.inputs.Value1}${this.getFunction(e.target.value)}${this.props.inputs.Value2}` },
+                this.props.cons))}}
+            value={this.props.inputs.Function}
+        >
+            <option>Add</option>
+            <option>Subtract</option>
+            <option>Multiply</option>
+            <option>Divide</option>
+        </select>
+    }
 
     static get input() {return {
-        Value1: 0,
-        Value2: 0
+        Function: 'Add',
+        Value1: '0.0',
+        Value2: '0.0'
     }}
 
     get show() {
@@ -159,7 +191,98 @@ class MathNode extends Node {
         }
     }
     static get output() {return {
-        Result: 0
+        Result: '0.0'
+    }}
+}
+
+class TrigNode extends Node {
+    get name() {return 'Trig'}
+    constructor(props) { super(props) }
+    getFunction (fun) {
+        switch (fun) {
+            case 'Sine':
+                return 'sin';
+            case 'Cosine':
+                return 'cos';
+            case 'Tangent':
+                return 'tan';
+        }
+        return 'Error';
+    }
+    getOutputs(inputs) { return { Function: `${this.getFunction(inputs.Function)}(${inputs.Value})` } }
+    center() {
+        return <select onChange={(e) => {
+            this.props.dispatch(Actions.updateNode(
+                this.props.id,
+                {Function: e.target.value },
+                {Function:`${this.getFunction(e.target.value)}(${this.props.inputs.Value})` },
+                this.props.cons))}}
+            value={this.props.inputs.Function}
+        >
+            <option>Sine</option>
+            <option>Cosine</option>
+            <option>Tangent</option>
+        </select>
+    }
+    static get input() {return {
+        Function: 'Sine',
+        Value: '0.0'
+    }}
+
+    get show() {
+        return {
+            inputs: {
+                Value: ''
+            },
+            outputs: {
+                Function: ''
+            }
+        }
+    }
+    static get output() {return {
+        Function: 'sin(0.0)'
+    }}
+}
+
+class PerlinNode extends Node {
+    get name() { return 'Perlin'; }
+    constructor(props) { super(props); }
+    getOutputs(inputs) { return 'perlin(vec3(position.x, position.y, 0.0))' }
+    center() {}
+    static get input() {return {
+    }}
+    get show() {
+        return {
+            inputs: {},
+            outputs: {
+                Value: ''
+            }
+        }
+    }
+    static get output() {return {
+        Value: 'perlin(position.x, position.y, 0.0)'
+    }}
+}
+
+class PositionNode extends Node {
+    get name() { return 'Position'; }
+    constructor(props) { super(props); }
+    getOutputs(inputs) { return PositionNode.output }
+    center() {}
+    static get input() {return {
+    }}
+    get show() {
+        return {
+            inputs: {},
+            outputs: {
+                X: '',
+                Y: ''
+            }
+        }
+    }
+    static get output() {return {
+        X: 'position.x',
+        Y: 'position.y'
     }}
 }
 
@@ -168,22 +291,133 @@ class OutputNode extends Node {
     constructor(props) { super(props); }
     getOutputs(inputs) {
         let Vertex = `
-        varying vec2 pos;
+        varying vec3 pos;
+        varying vec3 newPosition;
+        uniform int p[512];
+        float grad(int hash, float x, float y, float z) {
+            int h = int(mod(float(hash), 16.0));
+            float u = h < 8 ? x : y;
+            float v;
+            if(h < 4 /* 0b0100 */)
+                v = y;
+            else if(h == 12 /* 0b1100 */ || h == 14 /* 0b1110*/)
+                v = x;
+            else
+                v = z;
+            return (floor(mod(float(hash), 2.0)) == 0.0 ? u : -u)+(floor(mod(float(hash / 2), 2.0)) == 0.0 ? v : -v);
+        }
+        float fade(float t) {
+                                                        // Fade function as defined by Ken Perlin.  This eases coordinate values
+                                                        // so that they will ease towards integral values.  This ends up smoothing
+                                                        // the final output.
+            return t * t * t * (t * (t * 6.0 - 15.0) + 10.0); // 6t^5 - 15t^4 + 10t^3
+        }
+        int inc(int num) {
+            num++;
+
+            return num;
+        }
+        float perlin(float x, float y, float z) {
+            float repeat = 0.0;
+            if(repeat > 0.0) {
+                x = mod(x,repeat);
+                y = mod(y,repeat);
+                z = mod(z,repeat);
+            }
+
+            int xi = int(mod(x, 256.0));                              // Calculate the "unit cube" that the point asked will be located in
+            int yi = int(mod(y, 256.0));                              // The left bound is ( |_x_|,|_y_|,|_z_| ) and the right bound is that
+            int zi = int(mod(z, 256.0));                              // plus 1.  Next we calculate the location (from 0.0 to 1.0) in that cube.
+            float xf = fract(x);
+            float yf = fract(y);
+            float zf = fract(z);
+
+            float u = fade(xf);
+            float v = fade(yf);
+            float w = fade(zf);
+
+            int aaa, aba, aab, abb, baa, bba, bab, bbb;
+            aaa = p[p[p[    xi ]+    yi ]+    zi ];
+            aba = p[p[p[    xi ]+inc(yi)]+    zi ];
+            aab = p[p[p[    xi ]+    yi ]+inc(zi)];
+            abb = p[p[p[    xi ]+inc(yi)]+inc(zi)];
+            baa = p[p[p[inc(xi)]+    yi ]+    zi ];
+            bba = p[p[p[inc(xi)]+inc(yi)]+    zi ];
+            bab = p[p[p[inc(xi)]+    yi ]+inc(zi)];
+            bbb = p[p[p[inc(xi)]+inc(yi)]+inc(zi)];
+
+            float x1, x2, y1, y2;
+            x1 = mix(  grad (aaa, xf    , yf    , zf),           // The gradient function calculates the dot product between a pseudorandom
+                        grad (baa, xf-1.0, yf    , zf),             // gradient vector and the vector from the input coordinate to the 8
+                        u);                                     // surrounding points in its unit cube.
+            x2 = mix(  grad (aba, xf    , yf-1.0, zf),           // This is all then mixed together as a sort of weighted average based on the faded (u,v,w)
+                        grad (bba, xf-1.0, yf-1.0, zf),             // values we made earlier.
+                        u);
+            y1 = mix(x1, x2, v);
+            x1 = mix(  grad (aab, xf    , yf    , zf-1.0),
+                        grad (bab, xf-1.0, yf    , zf-1.0),
+                        u);
+            x2 = mix(  grad (abb, xf    , yf-1.0, zf-1.0),
+                        grad (bbb, xf-1.0, yf-1.0, zf-1.0),
+                        u);
+            y2 = mix (x1, x2, v);
+
+            return mix (y1, y2, w);                      // For convenience we bind the result to 0 - 1 (theoretical min/max before is [-1, 1])
+        }
         void main() {
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+            float offset = 0.0;
+            if (position.z > 0.1)
+                offset = min(${inputs.Height}, 10.0);
+            newPosition = vec3(position.xy,position.z + offset);
+            pos = (modelViewMatrix * vec4(newPosition, 1.0)).xyz;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition,1.0);
         }`
-        let Fragment = `void main(void) {
-            gl_FragColor = vec4(vec3(${inputs.Color}), 1.0);
+        let Fragment = `
+        varying vec3 pos;
+        varying vec3 newPosition;
+        void main(void) {
+            // vec3 N = normalize(cross(dFdx(pos), dFdy(pos)));
+            // vec3 L = normalize(vec3(5, 5, 5));
+            // vec4 diffuse = vec4(0.4, 0.4, 1.0, 1.0) * max(dot(L, N), 0.0);
+            // gl_FragColor = diffuse;
+            // float h = mod(newPosition.z, 1.0);
+            // vec3 color = (h >= 0.1) ? vec3(1.0) : vec3 (0.0);
+            // gl_FragColor = vec4(color, 1.0);
+            // vec3 f  = fract (newPosition * 3.0);
+            // vec3 df = fwidth(newPosition * 3.0);
+            //
+            // vec3 g = smoothstep(df * 1.0, df * 2.0, f);
+            //
+            // float c = g.z;
+            //
+            // gl_FragColor = vec4(c, c, c, 1.0);
+            vec3 P = newPosition;
+            float gsize = 10.0;
+            float gwidth = 1.0;
+            vec3 f  = abs(fract (P * gsize)-0.5);
+        	vec3 df = fwidth(P * gsize);
+        	float mi=max(0.0,gwidth-1.0), ma=max(1.0,gwidth);//should be uniforms
+        	vec3 g=clamp((f-df*mi)/(df*(ma-mi)),max(0.0,1.0-gwidth),1.0);//max(0.0,1.0-gwidth) should also be sent as uniform
+        	float c = g.z;
+            // vec3 N = normalize(cross(dFdx(pos), dFdy(pos)));
+            // vec3 L = normalize(vec3(5, 5, 5));
+            // vec4 diffuse = vec4(0.4, 0.4, 1.0, 1.0) * max(dot(L, N), 0.0);
+            // gl_FragColor = diffuse * c;
+        	gl_FragColor = vec4(vec3(newPosition.z / 2.0), 1.0);
         }`
         this.props.dispatch(Actions.setProgram(Vertex, Fragment))
     }
     center() {}
     static get input() {return {
-        Color: 0
+        Height: 0.0,
+        Color: 0.0
     }}
     get show() {
         return {
-            inputs: { Color: ''},
+            inputs: {
+                 Height: '',
+                 Color: ''
+             },
             outputs: {}
         }
     }
@@ -193,6 +427,9 @@ class OutputNode extends Node {
 
 export default {
     Value: connect()(ValueNode),
-    MathNode: connect()(MathNode),
+    Math: connect()(MathNode),
+    Trig: connect()(TrigNode),
+    Position: connect()(PositionNode),
+    Perlin: connect()(PerlinNode),
     Output: connect()(OutputNode)
 }
--- tool/Atlas/js/containers/Background.js
+++ tool/Atlas/js/containers/Background.js
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import { connect } from 'react-redux';
 import * as THREE from 'three'
@@ -13,58 +14,59 @@ class Background extends React.Component {
         this.renderer.domElement.style.zIndex = 0;
         this.renderer.domElement.className = "background";
         this.renderer.setClearColor(0x0e1112, 1);
-        // let geometry = new THREE.PlaneBufferGeometry(window.innerWidth, window.innerHeight);
-        //
-        // let material = new THREE.ShaderMaterial({
-        //     vertexShader: document.getElementById( 'vertexShader' ).textContent,
-        //     fragmentShader: document.getElementById( 'fragmentShader' ).textContent
-        // });
-        // this.background = new THREE.Mesh(geometry, material);
-        // this.background.rotation.x = -1.57;
-        // this.scene.add(this.background);
-        // this.background.position.y = 0;
-        //
-        // material = new THREE.ShaderMaterial({
-        //     vertexShader: props.Program.Vertex,
-        //     fragmentShader: props.Program.Fragment
-        // });
-        // let size = Math.min(window.innerWidth, window.innerHeight);
-        // geometry = new THREE.PlaneBufferGeometry(size, size, 256,  256);
-        // this.output = new THREE.Mesh(geometry, material);
-        // this.output.rotation.x = -1.57;
-        // this.scene.add(this.output);
-        // this.output.position.y = 0.1;
         var aspect = window.innerWidth / window.innerHeight;
-        var d = window.innerHeight;
-        this.camera = new THREE.OrthographicCamera(d * aspect / - 2, d * aspect / 2, d / 2, d / - 2, 1, 4000);
+        var d = 20.0;
+        this.camera = new THREE.OrthographicCamera(d * aspect / - 2, d * aspect / 2, d / 2, d / - 2, 0.1, 100);
         this.camera.position.set(0, d, 0);
         this.camera.lookAt(this.scene.position);
         this.camera.rotation.order = 'YXZ';
 
 
         let size = Math.max(d * aspect, d);
-        let geometry = new THREE.PlaneBufferGeometry(size * 2, size * 2);
+        let geometry = new THREE.PlaneBufferGeometry(size * 4, size * 4);
 
         let material = new THREE.ShaderMaterial({
             vertexShader: document.getElementById( 'vertexShader' ).textContent,
             fragmentShader: document.getElementById( 'fragmentShader' ).textContent
         });
+        let permutation = [ 151,160,137,91,90,15,                 // Hash lookup table as defined by Ken Perlin.  This is a randomly
+            131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,    // arranged array of all numbers from 0-255 inclusive.
+            190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+            88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+            77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+            102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+            135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+            5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+            223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+            129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+            251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+            49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+            138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
+        ];
+        let p = [];
+        for(let x=0;x<512;x++) {
+            p[x] = permutation[x%256];
+        }
         this.background = new THREE.Mesh(geometry, material);
         this.background.rotation.x = -1.57;
         this.scene.add(this.background);
-        this.background.position.y = 0;
+        this.background.position.y = -0.2;
 
         material = new THREE.ShaderMaterial({
+            uniforms: {
+                p: { type: "iv", value: p }
+            },
             vertexShader: props.Program.Vertex,
             fragmentShader: props.Program.Fragment
         });
+        console.log(material);
         size = Math.min(d * aspect, d);
         material.extensions.derivatives = true;
-        geometry = new THREE.BoxBufferGeometry( size, size, size/5, 64, 64, 1 );
+        geometry = new THREE.BoxBufferGeometry( size / 1.25, size / 1.25, 1.0, 64, 64, 1 );
         this.output = new THREE.Mesh(geometry, material);
         this.output.rotation.x = -1.57;
         this.scene.add(this.output);
-        this.output.position.y = 0.1;
+        this.output.position.y = 0.5;
 
         this.renderScene = this.renderScene.bind(this);
         this.onWindowResize = this.onWindowResize.bind(this);
@@ -94,18 +96,20 @@ class Background extends React.Component {
 
     renderScene() {
         if (this.transition) {
-            let d = window.innerHeight;
+            let d = 20.0;
             if (this.props.View == "3D") {
                 this.camera.position.x += d / 75.0;
+                this.camera.position.y += d / 300.0;
                 this.camera.position.z += d / 75.0;
                 this.camera.rotation.y +=  Math.PI / 300;
                 this.camera.rotation.x += ( Math.atan( - 1 / Math.sqrt( 2 ) ) - (- Math.PI / 2)) / 75;
-                if (this.camera.position.x >= window.innerHeight)
+                if (this.camera.position.x >= d)
                     this.transition = false;
 
             }
             else if (this.props.View == "2D") {
                 this.camera.position.x -= d / 75.0;
+                this.camera.position.y -= d / 300.0;
                 this.camera.position.z -= d / 75.0;
                 this.camera.rotation.y -=  Math.PI / 300;
                 this.camera.rotation.x -= ( Math.atan( - 1 / Math.sqrt( 2 ) ) - (- Math.PI / 2)) / 75;
@@ -118,8 +122,10 @@ class Background extends React.Component {
     }
 
     onWindowResize() {
-        let w = window.innerWidth;
-        let h = window.innerHeight;
+        var aspect = window.innerWidth / window.innerHeight;
+        var d = 20.0;
+        let w = d*aspect;
+        let h = d;
         this.camera.left = w / -2;
         this.camera.right = w / 2;
         this.camera.top = h / 2;
@@ -127,9 +133,9 @@ class Background extends React.Component {
         this.camera.updateProjectionMatrix();
 
         let size = Math.max(w, h);
-        this.background.geometry = new THREE.PlaneBufferGeometry(size * 2.5, size * 2.5);
-        size = Math.min(window.innerWidth, window.innerHeight);
-        this.output.geometry = new THREE.PlaneBufferGeometry(size, size, 256,  256);
+        this.background.geometry = new THREE.PlaneBufferGeometry(size * 4, size * 4);
+        size = Math.min(w, h);
+        this.output.geometry = new THREE.BoxBufferGeometry( size / 1.25, size / 1.25, 1.0, 64, 64, 1 );
         this.background.geometry.needsUpdate = true;
         this.background.geometry.attributes.position.needsUpdate = true;
         this.renderer.setSize(window.innerWidth, window.innerHeight);
